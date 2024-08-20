5a2d1e2400187ef0ebe14bcc96322844ff2da404
becamos97 <167942046+becamos97@users.noreply.github.com>
2024-08-20 19:29:23 -0400

    Add files via upload
    finished exercise

--- some and every exercices.js
+++ some and every exercices.js
@@ -2,8 +2,32 @@
 
 function hasOddNumber(arr) {
     return arr.some(num => num % 2 !== 0);
-}
+};
+
+//2. hasAZero
 
 function hasAZero(num) {
     return num.toString().split('').some(char)
-}
+};
+
+//3. hasOnlyOddNumbers
+
+function hasOnlyOddNumbers(arr) {
+    return arr.every(num => num % 2 !== 0);
+};
+
+//4, hasNoDuplicates
+function hasNoDuplicates(arr) {
+    return arr.length === new Set(arr).size;
+};
+
+//5. hasCertainKey
+function hasCertainKey(arr, key) {
+    return arr.every(obj => obj.hasOwnProperty(key));
+};
+
+//6. hasCertainValue
+function hasCertainValue(arr, key, value) {
+    return arr.every(obj => obj[key === value]);
+};
+
