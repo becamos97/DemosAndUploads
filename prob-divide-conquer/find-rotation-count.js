f81e073871329e14c13c3ca4c7360b52c845320c
becamos97 <167942046+becamos97@users.noreply.github.com>
2025-04-30 00:11:09 -0400

    Add files via upload

---
+++ prob-divide-conquer/find-rotation-count.js
@@ -0,0 +1,26 @@
+function findRotationCount(arr, low = 0, high = arr.length - 1) {
+    if (high < low) return 0;
+    if (high === low) return low;
+    let mid = Math.floor((low + high) / 2)
+
+    // Check if element (mid+1) is minimum element.
+    if (mid < high && arr[mid + 1] < arr[mid])
+        return mid + 1;
+
+  // Check if mid is minimum element
+    if (mid > low && arr[mid] < arr[mid - 1]) {
+        return mid;
+    }
+
+    // Decide whether we need to go to left half or
+    // right half
+    if (arr[high] > arr[mid]) {
+        return findRotationCount(arr, low, mid - 1);
+    }
+
+    return findRotationCount(arr, mid + 1, high);
+}
+  
+
+
+module.exports = findRotationCount
